cbe216365fceb787c75f4a8c8e26917abd41daf5
Jenna Perkin <45342929+ssesses@users.noreply.github.com>
2025-10-21 12:27:10 -0400

    set up lookat

--- Assets/Scripts/LookAt.cs
+++ Assets/Scripts/LookAt.cs
@@ -4,9 +4,7 @@ using UnityEngine;
 
 public class LookAt : MonoBehaviour
 {
-
     public Transform target;
-
     void Update()
     {
         transform.LookAt(target);
--- Assets/Scripts/RigiPlayerMovement.cs
+++ Assets/Scripts/RigiPlayerMovement.cs
@@ -7,15 +7,32 @@ public class RigiPlayerMovement : MonoBehaviour
     public Rigidbody rb;
     public Vector3 InputKey;
     public float moveSpeed;
+    public float jumpForce;
+
+    [Header("GroundDetection")]
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] float groundRadius;
+    [SerializeField] private LayerMask ground;
+    [SerializeField] private bool isGrounded;
 
     // Update is called once per frame
     void Update()
     {
+        //Checking Key Presses and Assigning Axis
         InputKey = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundRadius, (int)ground);
+
+        //Checking Jump Key and Jumping
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(0, jumpForce, 0);
+        }
     }
 
     void FixedUpdate()
     {
+        //Moving Player Based on Assigned Axis and Defined Speed
         rb.AddForce(InputKey * moveSpeed);
     }
 }
